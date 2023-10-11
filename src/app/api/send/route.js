4a3e2b17b11ffd60f5b84f5d9a921404dbe3efc9
Nacho Escobar <93625832+TuupaStyle@users.noreply.github.com>
2023-10-11 17:09:23 -0300

    retocando-pre-deploy

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -10,7 +10,7 @@ export async function POST(request) {
       secure: true,
       auth: {
         user: "contacto@lumideal.ar",
-        pass: "RxGDj@g5xK",
+        pass: process.env.PASS_MAIL,
       },
     });
 
@@ -39,41 +39,3 @@ export async function POST(request) {
     );
   }
 }
-
-// export default async function handler(req, res) {
-//   req.method = "POST";
-//   if (req.method === "POST") {
-//     const { nombre, email, mensaje } = req.body;
-
-//     try {
-//       // Configura el transportador de nodemailer (debes configurar tus propios detalles de SMTP)
-//       const transporter = nodemailer.createTransport({
-//         host: "smpt.mail.ferozo.com",
-//         port: 465,
-//         secure: false,
-//         auth: {
-//           user: "contacto@lumideal.ar",
-//           pass: "RxGDj@g5xK",
-//         },
-//       });
-
-//       // Configura el correo
-//       const mailOptions = {
-//         from: "no-reply@c1782292.ferozo.com",
-//         to: "contacto@lumideal.ar",
-//         subject: "Consulta de formulario de contacto",
-//         text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`,
-//       };
-
-//       // Envía el correo
-//       await transporter.sendMail(mailOptions);
-
-//       res.status(200).json({ message: "Correo enviado con éxito" });
-//     } catch (error) {
-//       console.error("Error al enviar el correo:", error);
-//       res.status(500).json({ message: "Error en el servidor" });
-//     }
-//   } else {
-//     res.status(405).json({ message: "Método no permitido" });
-//   }
-// }
